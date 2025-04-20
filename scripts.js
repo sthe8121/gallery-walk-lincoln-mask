8b0946dc608815a976b256b59b93f20c6a3d8701
sthe8121 <sandewt@gmail.com>
2025-04-20 13:11:01 +0700

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,16 +1,16 @@
-console.log('Lincoln Life Mask Gallery Loaded');
-function openAudioPopup() {
-  const popup = window.open('', 'Audio Guide', 'width=500,height=400');
-  popup.document.write(`
-    <html><head><title>Lincoln Audio Guide</title></head>
-    <body style="font-family:sans-serif; padding:1rem;">
-      <h3>Lincoln Tactile Audio Guide</h3>
-      <ul>
-        <li><a href="https://npg.si.edu/sites/default/files/audio/lincoln_intro_orientation.mp3" target="_blank">Introduction and Orientation</a></li>
-        <li><a href="https://npg.si.edu/sites/default/files/audio/lincoln_1860_mask.mp3" target="_blank">1860 Face Mask</a></li>
-        <li><a href="https://npg.si.edu/sites/default/files/audio/lincoln_1865_mask.mp3" target="_blank">1865 Face Mask</a></li>
-        <li><a href="https://npg.si.edu/sites/default/files/audio/lincoln_hands.mp3" target="_blank">Lincoln Hands</a></li>
-      </ul>
-    </body></html>
+
+function playAudio(audioUrl) {
+  const audioWindow = window.open('', 'AudioPlayer', 'width=400,height=200');
+  audioWindow.document.write(`
+    <html>
+    <head><title>Playing Audio</title></head>
+    <body style="font-family:Arial;padding:1rem;">
+      <p>Now playing audio:</p>
+      <audio controls autoplay>
+        <source src="${audioUrl}" type="audio/mpeg">
+        Your browser does not support the audio element.
+      </audio>
+    </body>
+    </html>
   `);
 }
